5a7bd4d0bacdd74fd391cd5ee45f2d8d566d4ddc
AmorimY <rafael.amorim.praia@gmail.com>
2024-06-27 03:41:36 -0300

    version 1.0

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,12 +1,16 @@
 import axios from "axios";
 import Table from "../components/table";
 import { useEffect, useState } from "react";
-export default function Home(){
-    return(
-
-        <div>
-            
-            <Table/>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
+      <Table />
+    </div>
+  );
 }
